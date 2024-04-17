2de4953d83ab015066ad9bdaed362af2e8743893
Johlmike <johlmike@gmail.com>
2024-04-17 22:45:48 +0800

    與 MongoDB Atlas 連線

--- server.js
+++ server.js
@@ -1,43 +1,58 @@
 const http = require('http');
+const mongoose = require('mongoose'); // MongoDB
+require('dotenv').config(); // .env
 const { v4: uuidv4 } = require('uuid');
 const errHandle = require('./errorHandle');
 const todos = [];
 
-const requestListener = (req, res)=>{
-    const headers = {
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
-        'Content-Type': 'application/json'
-    }
-    let body = "";
-    
-    req.on('data', chunk=>{
-        body+=chunk;
-    })
-    
-    if(req.url=="/todos" && req.method == "GET"){
-        // getTodo.js
-    }else if(req.url=="/todos" && req.method == "POST"){
-        // postTodo.js
-    }else if(req.url=="/todos" && req.method == "DELETE"){
-        // deleteTodo.js
-    }else if(req.url.startsWith("/todos/") && req.method=="DELETE"){
-        // deleteTodo.js
-    }else if(req.url.startsWith("/todos/") && req.method=="PATCH"){
-        // patchTodo.js
-    }else if(req.method == "OPTIONS"){
-        res.writeHead(200,headers);
-        res.end();
-    }else{
-        res.writeHead(404,headers);
-        res.write(JSON.stringify({
-            "status": "false",
-            "message": "無此網站路由"
-        }));
-        res.end();
-    }
-}
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const requestListener = (req, res) => {
+  const headers = {
+    'Access-Control-Allow-Headers':
+      'Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+    'Content-Type': 'application/json',
+  };
+  let body = '';
+
+  req.on('data', (chunk) => {
+    body += chunk;
+  });
+
+  if (req.url == '/todos' && req.method == 'GET') {
+    // getTodo.js
+  } else if (req.url == '/todos' && req.method == 'POST') {
+    // postTodo.js
+  } else if (req.url == '/todos' && req.method == 'DELETE') {
+    // deleteTodo.js
+  } else if (req.url.startsWith('/todos/') && req.method == 'DELETE') {
+    // deleteTodo.js
+  } else if (req.url.startsWith('/todos/') && req.method == 'PATCH') {
+    // patchTodo.js
+  } else if (req.method == 'OPTIONS') {
+    res.writeHead(200, headers);
+    res.end();
+  } else {
+    res.writeHead(404, headers);
+    res.write(
+      JSON.stringify({
+        status: 'false',
+        message: '無此網站路由',
+      })
+    );
+    res.end();
+  }
+};
 
 const server = http.createServer(requestListener);
 server.listen(process.env.PORT || 3005);
