78bdebf216743f95037ab72d919f0bf3642ef9cf
wting04 <purebbt@gmail.com>
2024-04-27 22:36:44 +0800

    fixed:Todo.findIndex is not a function

--- patchTodo.js
+++ patchTodo.js
@@ -4,18 +4,18 @@ const Todo = require('./model/todo');
 
 const patchTodo = async ({ body, req, res }) => {
     try {
-        const todo = JSON.parse(body).title; //AS POST
+        const todo = JSON.parse(body); //AS POST
         const id = req.url.split('/').pop(); //AS DELETE{id}
-        const index = Todo.findIndex(element => element.id == id); //AS DELETE{id}
-      
-      if(todo !== undefined && index !== -1){
+        //更新單筆
+        await Todo.findByIdAndUpdate(id,todo,{new: true,})
+        .then((result)=>{
+          //option開啟new 可回傳修改成功的資料
+          handleSuccess(res, result);
+        })
+        .catch(error => {
+          handleError(res, error);
+        });         
 
-        const result = await Todo.findByIdAndUpdate(id,todo); //更新單筆
-        handleSuccess(res, result);
- 
-        }else{
-            handleError(res, error);
-        }
     } catch (error) {
       handleError(res, error);
     }
