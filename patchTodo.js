06f1d9e3a43b9088562ab71f3cbed8b568cc1fc7
wting04 <purebbt@gmail.com>
2024-04-29 17:56:37 +0800

    優化程式

--- patchTodo.js
+++ patchTodo.js
@@ -7,15 +7,20 @@ const patchTodo = async ({ body, req, res }) => {
         const todo = JSON.parse(body); //AS POST
         const id = req.url.split('/').pop(); //AS DELETE{id}
         //更新單筆
-        await Todo.findByIdAndUpdate(id,todo,{new: true,})
-        .then((result)=>{
-          //option開啟new 可回傳修改成功的資料
-          handleSuccess(res, result);
-        })
-        .catch(error => {
-          handleError(res, error);
-        });         
+        if(todo.title !== undefined ) {   
+          //option開啟new 可回傳修改成功的資料、開啟runValidators 作更新資料的驗證
+          const result = await Todo.findByIdAndUpdate(id,todo,{new: true, runValidators: true});
+          if(result !== null){
+            handleSuccess(res, result);
+          }
+          else{
+            handleError(res, {'message' : '查無此ID'});
+          } 
 
+        }else{
+          handleError(res, { "message": "欄位填寫錯誤"});
+        }  
+    
     } catch (error) {
       handleError(res, error);
     }
